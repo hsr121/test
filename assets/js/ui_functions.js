2ddf00970a08c4316e8b4ac076cc28398010b78e
Harshal Singh Raushan <hsraushan@gmail.com>
2022-11-01 01:24:51 +0530

    te

--- assets/js/ui_functions.js
+++ assets/js/ui_functions.js
@@ -228,23 +228,23 @@ const populateFounder = () => {
         };
         card.addEventListener('click', fun, false);
     });
-    $("#slider").on('touchstart', function (event) {
-        const xClick = event.originalEvent.touches[0].pageX;
-        $(this).one('touchmove', function (event) {
-            const xMove = event.originalEvent.touches[0].pageX;
-            const sensitivityInPx = 5;
+    // $("#slider").on('touchstart', function (event) {
+    //     const xClick = event.originalEvent.touches[0].pageX;
+    //     $(this).one('touchmove', function (event) {
+    //         const xMove = event.originalEvent.touches[0].pageX;
+    //         const sensitivityInPx = 5;
 
-            if (Math.floor(xClick - xMove) > sensitivityInPx) {
-                $(this).carousel('next');
-            }
-            else if (Math.floor(xClick - xMove) < -sensitivityInPx) {
-                $(this).carousel('prev');
-            }
-        });
-        $(this).on('touchend', function () {
-            $(this).off('touchmove');
-        });
-    });
+    //         if (Math.floor(xClick - xMove) > sensitivityInPx) {
+    //             $(this).carousel('next');
+    //         }
+    //         else if (Math.floor(xClick - xMove) < -sensitivityInPx) {
+    //             $(this).carousel('prev');
+    //         }
+    //     });
+    //     $(this).on('touchend', function () {
+    //         $(this).off('touchmove');
+    //     });
+    // });
 }
 
 const populateRoadmap = () => {
